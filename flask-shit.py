96251a02b73b91d31437f4aab2437b4f35901922
Matt Hardwick <matt.bellhops@getbellhops.com>
2014-08-16 13:52:32 -0400

    Image uploads accept face images.

--- flask-shit.py
+++ flask-shit.py
@@ -1,15 +1,29 @@
 from flask import Flask
 from flask import request
+
 app = Flask('walkout')
 
+
 from audio import Audio
+import os
 
 a = Audio()
 a.convert_mp3_to_wav('run.mp3')
 
+app.config['UPLOAD_FOLDER'] = 'media/image/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 @app.route("/face", methods=['POST'])
 def face_dump():
-
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        # import ipdb; ipdb.set_trace()
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
     return ''
 
 
