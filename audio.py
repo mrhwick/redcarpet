8d6287b1e8c704ada2c44a5576cc2c624aa07b0e
Matt Hardwick <matt.bellhops@getbellhops.com>
2014-08-16 13:40:31 -0400

    threaded playback

--- audio.py
+++ audio.py
@@ -1,6 +1,8 @@
 
+import os
 import pyaudio
 from pydub import AudioSegment
+from threading import Thread
 import wave
 
 
@@ -8,28 +10,59 @@ class Audio(object):
 
     def __init__(self):
         self.block = 4
+        self.thread = Thread(target=self.do_playback)
+        self.playlist = []
 
-    def play(self, filename):
-        wf = wave.open(filename, 'rb')
+    @property
+    def is_playing(self):
+        return self.thread.is_alive()
 
-        p = pyaudio.PyAudio()
+    def play(self):
+        if not self.is_playing:
+            self.thread = Thread(target=self.do_playback)
+            self.thread.start()
 
-        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                        channels=wf.getnchannels(),
-                        rate=wf.getframerate(),
-                        output=True)
+    def do_playback(self):
 
-        data = wf.readframes(self.block)
+        while len(self.playlist) > 0:
 
-        while data != '':
-            stream.write(data)
+            wf = wave.open('media/wav/' + self.playlist.pop(), 'rb')
+
+            p = pyaudio.PyAudio()
+
+            stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                            channels=wf.getnchannels(),
+                            rate=wf.getframerate(),
+                            output=True)
+
             data = wf.readframes(self.block)
+
+            while data != '':
+                stream.write(data)
+                data = wf.readframes(self.block)
 
         stream.stop_stream()
         stream.close()
 
         p.terminate()
 
+    def add_to_playlist(self, filename):
+        self.playlist.insert(0, filename)
+
     def convert_mp3_to_wav(self, mp3_filename):
-        sound_file = AudioSegment.from_mp3(mp3_filename)
-        sound_file.export('sound.wav', format='wav')
+
+        # Get the mp3 data.
+
+        sound_file = AudioSegment.from_mp3('media/mp3/' + mp3_filename)
+
+        # Slice the file extension.
+
+        filename_less_extension = os.path.splitext(mp3_filename)[0]
+
+        # Give it a wav extension.
+
+        wav_filename = filename_less_extension + '.wav'
+
+        # Export the wav version for playback.
+
+        sound_file.export('media/wav/' + wav_filename, format='wav')
--- flask-shit.py
+++ flask-shit.py
@@ -1,15 +1,28 @@
 from flask import Flask
+from flask import request
 app = Flask('walkout')
 
 from audio import Audio
 
-@app.route("/")
-def hello():
-    print('hello')
-    a = Audio()
-    a.convert_mp3_to_wav('run.mp3')
-    a.play('sound.wav')
-    return 'yo'
+a = Audio()
+a.convert_mp3_to_wav('run.mp3')
+
+@app.route("/face", methods=['POST'])
+def face_dump():
+
+    return ''
+
+
+@app.route("/play/<filename>")
+def play_input(filename):
+    print('playing ' + filename)
+
+    if a.is_playing:
+        print('adding to playlist.')
+
+    a.add_to_playlist(filename)
+    a.play()
+    return 'playing ' + filename
 
 if __name__ == "__main__":
-  app.run()
+  app.run(host='0.0.0.0')
